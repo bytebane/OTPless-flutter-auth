255094ab0c114a5e8169614a612303ee674a2fbe
ByteBane <81875670+bytebane@users.noreply.github.com>
2024-01-01 03:06:40 +0530

    +++ otpless-auth

--- android/app/src/main/kotlin/com/bytebane/otpless_auth/MainActivity.kt
+++ android/app/src/main/kotlin/com/bytebane/otpless_auth/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.bytebane.otpless_auth
 
 import io.flutter.embedding.android.FlutterActivity
+//Import the following two for OTPless Flutter Plugin
+import com.otpless.otplessflutter.OtplessFlutterPlugin;
+import android.content.Intent;
 
 class MainActivity: FlutterActivity() {
+
+// OTPless Flutter Plugin implementation new Intent and Back Press Handling Functions
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
+        if (plugin is OtplessFlutterPlugin) {
+            plugin.onNewIntent(intent)
+        }
+    }
+    override fun onBackPressed() {
+        val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
+        if (plugin is OtplessFlutterPlugin) {
+            if (plugin.onBackPressed()) return
+        }
+    // handle other cases
+        super.onBackPressed()
+    }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:otpless_auth/widgets/my_button.dart';
+import 'package:otpless_auth/widgets/my_text.dart';
+import 'package:otpless_flutter/otpless_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,29 +11,105 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isLoggedIn = false;
+  bool isError = false;
+  String errorMessage = "";
+
+  Object userData = {
+    "token": null,
+    "timestamp": null,
+    "timezone": null,
+    "mobile": {"name": null, "number": null},
+    "email": {"name": null, "email": null},
+  };
+
+  final _otpLessFlutterPlugin = Otpless();
+
+  // Open Login Page from OTPless
+  Future<void> openLoginPage() async {
+    _otpLessFlutterPlugin.openLoginPage(
+      (result) {
+        if (result['data'] != null) {
+          // OTPless User details received in the callback
+          setState(() {
+            isLoggedIn = true;
+            userData = result['data'];
+          });
+
+          // You can send result['data']['token'] to your backend service to validate
+        } else {
+          // Handle error
+          setState(() {
+            isError = true;
+            errorMessage = result['errorMessage'];
+          });
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: const Text('OTPless Auth'),
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/bg.png"),
-              fit: BoxFit.fill,
-            ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text('OTPless Auth'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/bg.png"),
+            fit: BoxFit.fill,
           ),
-          child: Column(children: [
-            Expanded(
-              child: Center(
-                  child: TextButton(
-                onPressed: () {},
-                child: const Text('Get Started'),
-              )),
-            )
-          ]),
-        ));
+        ),
+        child: Column(
+          children: [
+            isLoggedIn
+                ? Expanded(
+                    child: Center(
+                      child: Text(
+                        userData.toString(),
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: Center(
+                      child: MyButton(
+                        onPressed: () {
+                          openLoginPage();
+                        },
+                        type: MyButtonType.text,
+                        child: const Text('Get Started'),
+                      ),
+                    ),
+                  ),
+            isError
+                ? Center(
+                    child: MyText(
+                      text: errorMessage,
+                      type: MyTextType.error,
+                    ),
+                  )
+                : Container(),
+            isLoggedIn
+                ? MyButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          isLoggedIn = false;
+                          isError = false;
+                          errorMessage = "";
+                        },
+                      );
+                    },
+                    type: MyButtonType.elevated,
+                    child: const Text('Logout'),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
   }
 }
